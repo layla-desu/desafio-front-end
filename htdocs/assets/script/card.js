050c07f1bb3efe48f68dc4ee73961db95fcbe0a2
Layla K <lhuguenim@edglobo.com.br>
2018-03-02 20:21:17 -0300

    Add style for share link

--- htdocs/assets/script/card.js
+++ htdocs/assets/script/card.js
@@ -1,5 +1,3 @@
-//dataN = '';
-
 function fetchJSON(path, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', path, true);
@@ -30,8 +28,6 @@ fetchJSON('../../data.json', function(data){
     
     //var node = document.getElementById('content').lastChild;
     //content.appendChild(markup(data.section[0]));
-    
-    //markup(dataN);
 })
 
 function markup(section) {
@@ -44,19 +40,20 @@ function markup(section) {
     ${sectionTitle}
         ${section.data.map((item, index) => `
             <section class="item">
-                <div>
-                    <a href="${item.url}">
-                        <figure>
-                                <img class="img-${index}" src="./assets/media/${item.image}" alt="${item.title}">
-                        </figure>
-                    </a>
+                <div id="card-${index}">
+                    <figure>
+                            <img class="img-${index}" src="./assets/media/${item.image}" alt="${item.title}">
+                    </figure>
                     <div>
                         <h3>${item.label}</h3>
                         <a href="${item.url}">
                             <h2>${item.title}</h2>
                         </a>
                         <p>${item.description}</p>
-                    </div>                   
+                    </div>
+                    <a href="${item.url}">
+                        LINK
+                    </a>
                 </div>
             </section>
         `).join('')}
@@ -75,69 +72,3 @@ function markupAdv()  {
         </section>
     `;
 }
-
-
-
-
-
-/*
-import {CardView} from './CardView';
-
-function getData() {
-    console.log('deu certo');
-    CardView cv = new CardView();
-}
-
-getListContent() {
-    let limit = this.getLimit();
-    if (this.reserve.hasItems(limit)) {
-        return Promise.resolve(
-            JSON.stringify(this.reserve.get(limit))
-        );
-    }
-    let init = this.getInit();
-    let upperLimit = (limit * this.bigStep) + 1;
-    let listUrl = [
-        this.url,
-        `?section=${this.section}`,
-        `&limit=${upperLimit}`,
-        `&init=${init}`
-    ].join('');
-    return utils.ajax('GET', listUrl).then(
-        result => this.dealWithList(result, limit)
-    );
-}
-
-
-
-
-
-/*
-var data = JSON.parse(data);
-
-function loadJSON(callback) {
-
-var xobj = new XMLHttpRequest();
-xobj.overrideMimeType("application/json");
-xobj.open('GET', 'file.json', true);
-xobj.onreadystatechange = function () {
-if (xobj.readyState == 4 && xobj.status == "200") {
-
-// .open will NOT return a value but simply returns undefined in async mode so use a callback
-callback(xobj.responseText);
-
-}
-}
-xobj.send(null);
-
-}
-
-// Call to function with anonymous callback
-loadJSON(function(response) {
-// Do Something with the response e.g.
-//jsonresponse = JSON.parse(response);
-
-// Assuming json data is wrapped in square brackets as Drew suggests
-//console.log(jsonresponse[0].name);
-
-}); */
